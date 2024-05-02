88d1f598a91475c06ad16f873dcfe144a16cfe56
Mervin Choun <mchoun@paypal.com>
2024-05-02 10:50:52 -0400

    adding lazyvim to work

--- work/nvim/init.lua
+++ work/nvim/init.lua
@@ -1,6 +1,2 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.opt.clipboard = "unnamed"
-require "user.keymaps"
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
