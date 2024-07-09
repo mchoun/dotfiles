29069bf56e0f38fa86cd2de4be05a235085b3552
Mervin Choun <mchoun@paypal.com>
2024-07-09 13:59:46 -0400

    add last window for tmux and readme bat alias for zshrc

--- work/nvim/lua/plugins/example.lua
+++ work/nvim/lua/plugins/example.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+-- if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -28,7 +28,7 @@ return {
   },
 
   -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
+  { "folke/trouble.nvim", enabled = true },
 
   -- override nvim-cmp and add cmp-emoji
   {
@@ -127,8 +127,6 @@ return {
         "lua",
         "markdown",
         "markdown_inline",
-        "python",
-        "query",
         "regex",
         "tsx",
         "typescript",
