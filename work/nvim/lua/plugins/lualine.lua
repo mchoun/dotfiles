03739eb552d2ced469f1f5a9d60c710e4fc18a84
Mervin Choun <mchoun@paypal.com>
2025-02-12 16:01:29 -0500

    add winbar file name

---
+++ work/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+
+      inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+    },
+  },
+}
