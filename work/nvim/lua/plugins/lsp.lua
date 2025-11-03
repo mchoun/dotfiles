baf9647a1bd0db41b3d46cb915ee1e754a0000f3
Mervin Choun <mchoun@paypal.com>
2025-11-03 13:25:40 -0500

    update 2025-11-03 - changes to config to handle updated lazyvim

--- work/nvim/lua/plugins/lsp.lua
+++ work/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,20 @@
 return {
-  { "neovim/nvim-lspconfig", opts = {
-    inlay_hints = { enabled = false },
-  } },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        flow = {},
+      },
+    },
+  },
   { "bennypowers/template-literal-comments.nvim", opts = true, ft = {
     "javascript",
     "typescript",
+  } },
+  { "stevearc/conform.nvim", opts = {
+    formatters_by_ft = {
+      ejs = { "prettier" },
+    },
   } },
 }
--- work/nvim/lua/plugins/treesitter.lua
+++ work/nvim/lua/plugins/treesitter.lua
@@ -1,43 +1,42 @@
 return {
-
-  -- add more treesitter parsers
+  --   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "embedded_template",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-    },
+    --     opts = {
+    --       ensure_installed = {
+    --         "bash",
+    --         "html",
+    --         "javascript",
+    --         "embedded_template",
+    --         "json",
+    --         "lua",
+    --         "markdown",
+    --         "markdown_inline",
+    --         "regex",
+    --         "tsx",
+    --         "typescript",
+    --         "vim",
+    --         "yaml",
+    --       },
+    --     },
     -- ejs fuckery to get it to work
     config = function(_, opts)
       vim.treesitter.language.register("embedded_template", "ejs")
-      require("nvim-treesitter.configs").setup(opts)
+      -- require("nvim-treesitter.configs").setup(opts)
     end,
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      vim.filetype.add({
-        extension = { ejs = "ejs" },
-      })
-
-      opts.servers = opts.servers or {}
-      opts.servers.html = {
-        filetypes = { "html", "ejs" },
-      }
-      return opts
-    end,
-  },
+  --   {
+  --     "neovim/nvim-lspconfig",
+  --     opts = function(_, opts)
+  --       vim.filetype.add({
+  --         extension = { ejs = "ejs" },
+  --       })
+  --
+  --       opts.servers = opts.servers or {}
+  --       opts.servers.html = {
+  --         filetypes = { "html", "ejs" },
+  --       }
+  --       return opts
+  --     end,
+  --   },
 }
